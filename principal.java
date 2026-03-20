b20c03c3d4f78262e650300eb23f9860b1fd2058
Pablo <pablochavezibrra90@gmail.com>
2026-03-20 21:41:40 +0100

    Añadida funcionalidad de fecha y hora

--- principal.java
+++ principal.java
@@ -1,5 +1,9 @@
+import java.time.LocalDateTime;
+
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        
+        System.out.println("La fecha y hora actual es: " + LocalDateTime.now());
     }
 }
